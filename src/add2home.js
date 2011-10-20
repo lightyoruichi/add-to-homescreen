9a7c5f4f45a2e3ee587b32db3c6cbb0ff3a4b1a4
Harinder Singh <lightyoruichi@ktard.co.cc>
2011-10-20 08:36:35 +0800

    Settings for CDN-ing

--- src/add2home.js
+++ src/add2home.js
@@ -19,12 +19,12 @@ var nav = navigator,
 	options = {
 		animationIn: 'drop',		// drop || bubble || fade
 		animationOut: 'fade',		// drop || bubble || fade
-		startDelay: 2000,			// 2 seconds from page load before the balloon appears
+		startDelay: 1000,			// 2 seconds from page load before the balloon appears
 		lifespan: 20000,			// 20 seconds before it is automatically destroyed
 		bottomOffset: 14,			// Distance of the balloon from bottom
 		expire: 0,					// Minutes to wait before showing the popup again (0 = always displayed)
 		message: '',				// Customize your message or force a language ('' = automatic)
-		touchIcon: false,			// Display the touch icon
+		touchIcon: true,			// Display the touch icon
 		arrow: true,				// Display the balloon arrow
 		iterations:100				// Internal/debug use
 	},
